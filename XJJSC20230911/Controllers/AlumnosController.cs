59f2c1de2dc230f76ef04d3e43c89e3457697cd7
XiomaraCamp <jc9971859@gmailcom>
2023-09-11 22:37:52 -0600

    Creacion de Controller y Models

---
+++ XJJSC20230911/Controllers/AlumnosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace XJJSC20230911.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        // GET: api/<AlumnosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AlumnosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AlumnosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AlumnosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AlumnosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ XJJSC20230911/Models/Alumno.cs
@@ -0,0 +1,9 @@
+﻿namespace XJJSC20230911.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string Nota { get; set; }
+    }
+}
